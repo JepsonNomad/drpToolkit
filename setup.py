8baec5deaaf8ab9ff6793b069e09170b0d64bf6e
Christian John <cjohn@ucdavis.edu>
2021-04-20 15:12:17 -0700

    setup files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='drpToolkit',
+    url='https://github.com/jepsonnomad/drpToolkit',
+    author='Christian John',
+    author_email='cjohn@ucdavis.edu',
+    packages=['drpToolkit'],
+    install_requires=['numpy'],
+    version='0.0.0.9000',
+    license='Gnu GPL 3.0',
+    description='Digital repeat photography imagery management and analysis',
+    long_description=open('README.md').read(),
+    scripts=['drpToolkit_scripts/prep','drpToolkit_scripts/align','drpToolkit_scripts/extract','drpToolkit_scripts/panelize']
+)
