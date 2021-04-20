074facb3923028f3f714b39f089fea5e1c5d9afc
Christian John <cjohn@ucdavis.edu>
2021-04-20 15:51:07 -0700

    setup updates

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author='Christian John',
     author_email='cjohn@ucdavis.edu',
     packages=['drpToolkit'],
+    package_dir={'drpToolkit': 'drpToolkit'},
     install_requires=['numpy'],
     version='0.0.0.9000',
     license='Gnu GPL 3.0',
