f4bf940d6e47f00cf0f7c8089ed51859e6f6e281
Christian John <cjohn@ucdavis.edu>
2021-09-20 10:49:54 -0700

    versioning and citation update

--- setup.py
+++ setup.py
@@ -6,16 +6,10 @@ setup(
     author='Christian John',
     author_email='cjohn@ucdavis.edu',
     packages=['drpToolkit'],
-    package_dir={'drpToolkit': 'drpToolkit'},
     install_requires=['numpy'],
-    version='0.0.1',
+    version='0.1.0',
     license='Gnu GPL 3.0',
     description='Digital repeat photography imagery management and analysis',
     long_description=open('README.md').read(),
-    entry_points = {
-        'console_scripts': ['prep=drpToolkit.prep:main',
-        'align=drpToolkit.align:main',
-        'extract=drpToolkit.extract:main',
-        'panelize=drpToolkit.panelize:main']
-    }
+    scripts=['drpToolkit_scripts/prep','drpToolkit_scripts/align','drpToolkit_scripts/extract','drpToolkit_scripts/panelize']
 )
